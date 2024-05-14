b27b86e2d1960213aa3e18bf4cbcfd410b4feb3e
JiYoung <yunje26@gmail.com>
2024-05-14 09:24:30 +0900

    test

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Map[10][10] =
+	{
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	};
+
+	int PlayerX = 1;
+	int PlayerY = 1;
+	int PlayerA = 8;
+	int PlayerB = 8;
+	char Key = 0;
+
+	system("cls");
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			if (PlayerX == X && PlayerY == Y)
+			{
+				cout << "P";
+			}
+			else if (PlayerA == X && PlayerB == Y)
+			{
+				cout << "G";
+			}
+			else if (Map[Y][X] == 0)
+			{
+				cout << " ";
+			}
+			else if (Map[Y][X] == 1)
+			{
+				cout << "*";
+			}
+		}
+		cout << endl;
+	}
+
+
+
+
+	return 0;
+}
